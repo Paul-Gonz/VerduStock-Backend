15e5785735fe5e8c691228a88b6d1d0fb0af5ed6
Paul-Gonz <gonzalezpaul204@gmail.com>
2026-01-30 18:38:46 -0400

    api-test

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 🔹 RUTAS DE PROVEEDORES (las que ya tienes)
+Route::get('/api-test', function () {
+    return response()->json([
+        'status' => 'Conectado',
+        'message' => 'Nuxt 4 está hablando con Laravel',
+        'time' => now()
+    ]);
+});
+
 Route::prefix('proveedores')->group(function () {
     Route::get('/', [ProveedoresController::class, 'index']);
     Route::post('/', [ProveedoresController::class, 'store']);
@@ -21,5 +28,5 @@ Route::prefix('categorias')->group(function () {
     Route::post('/', [CategoriaController::class, 'store']);
     Route::get('/{id}', [CategoriaController::class, 'show']);
     Route::put('/{id}', [CategoriaController::class, 'update']);
-    Route::delete('/{id}', [CategoriaController::class, 'destroy']);
+    //Route::delete('/{id}', [CategoriaController::class, 'destroy']);
 });
