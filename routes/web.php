52653a2065d8b81019a25276427ca08fecf226a5
Paul-Gonz <gonzalezpaul204@gmail.com>
2026-02-06 17:26:20 -0400

    se agrego proteccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -18,41 +18,6 @@ Route::get('/api-test', function () {
         'time' => now()
     ]);
 });
-
-Route::prefix('proveedores')->group(function () {
-    Route::get('/', [ProveedoresController::class, 'index']);
-    Route::post('/', [ProveedoresController::class, 'store']);
-    Route::get('/{id}', [ProveedoresController::class, 'show']);
-    Route::put('/{id}', [ProveedoresController::class, 'update']);
-});
-
-// 🔹 RUTAS DE CATEGORÍAS (las que ya tienes)
-Route::prefix('categorias')->group(function () {
-    Route::get('/', [CategoriaController::class, 'index']);
-    Route::post('/', [CategoriaController::class, 'store']);
-    Route::get('/{id}', [CategoriaController::class, 'show']);
-    Route::put('/{id}', [CategoriaController::class, 'update']);
-    //Route::delete('/{id}', [CategoriaController::class, 'destroy']);
-});
-
-// 🔹 NUEVAS RUTAS DE PRODUCTOS (CRUD completo)
-Route::prefix('productos')->group(function () {
-    // CRUD básico
-    Route::get('/', [ProductoController::class, 'index'])->name('productos.index');
-    Route::get('/crear', [ProductoController::class, 'create'])->name('productos.create');
-    Route::post('/', [ProductoController::class, 'store'])->name('productos.store');
-    Route::get('/{id}', [ProductoController::class, 'show'])->name('productos.show');
-    Route::get('/{id}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
-    Route::put('/{id}', [ProductoController::class, 'update'])->name('productos.update');
-    Route::delete('/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
-    
-    // Rutas adicionales
-    Route::get('/reporte/estadisticas', [ProductoController::class, 'reporte'])->name('productos.reporte');
-    Route::get('/categoria/{categoriaId}', [ProductoController::class, 'porCategoria'])->name('productos.por-categoria');
-    Route::get('/alto-desperdicio', [ProductoController::class, 'altoDesperdicio'])->name('productos.alto-desperdicio');
-    
-  });
-
     //Login/Logout
 
 // 🔹 RUTAS PÚBLICAS DE AUTENTICACIÓN
@@ -86,7 +51,43 @@ Route::middleware(['auth'])->group(function () {
             ]
         ]);
     })->name('dashboard');
+
+        
+    Route::prefix('proveedores')->group(function () {
+        Route::get('/', [ProveedoresController::class, 'index']);
+        Route::post('/', [ProveedoresController::class, 'store']);
+        Route::get('/{id}', [ProveedoresController::class, 'show']);
+        Route::put('/{id}', [ProveedoresController::class, 'update']);
+    });
+
+    // 🔹 RUTAS DE CATEGORÍAS (las que ya tienes)
+    Route::prefix('categorias')->group(function () {
+        Route::get('/', [CategoriaController::class, 'index']);
+        Route::post('/', [CategoriaController::class, 'store']);
+        Route::get('/{id}', [CategoriaController::class, 'show']);
+        Route::put('/{id}', [CategoriaController::class, 'update']);
+        //Route::delete('/{id}', [CategoriaController::class, 'destroy']);
+    });
+
+    // 🔹 NUEVAS RUTAS DE PRODUCTOS (CRUD completo)
+    Route::prefix('productos')->group(function () {
+        // CRUD básico
+        Route::get('/', [ProductoController::class, 'index'])->name('productos.index');
+        Route::get('/crear', [ProductoController::class, 'create'])->name('productos.create');
+        Route::post('/', [ProductoController::class, 'store'])->name('productos.store');
+        Route::get('/{id}', [ProductoController::class, 'show'])->name('productos.show');
+        Route::get('/{id}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
+        Route::put('/{id}', [ProductoController::class, 'update'])->name('productos.update');
+        Route::delete('/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+        
+        // Rutas adicionales
+        Route::get('/reporte/estadisticas', [ProductoController::class, 'reporte'])->name('productos.reporte');
+        Route::get('/categoria/{categoriaId}', [ProductoController::class, 'porCategoria'])->name('productos.por-categoria');
+        Route::get('/alto-desperdicio', [ProductoController::class, 'altoDesperdicio'])->name('productos.alto-desperdicio');
+        
+    });
+
 });
 
-Route::get('/login-form', [AuthController::class, 'showLoginForm'])->name('login.form');
-Route::get('/register-form', [AuthController::class, 'showRegisterForm'])->name('register.form');
+//Route::get('/login-form', [AuthController::class, 'showLoginForm'])->name('login.form');
+//Route::get('/register-form', [AuthController::class, 'showRegisterForm'])->name('register.form');
