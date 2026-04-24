d78479456875856e6aa25aa5b4b8b7fda5db4b27
Paul-Gonz <gonzalezpaul204@gmail.com>
2026-04-24 00:59:33 -0400

    fix session error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,18 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
+        // Habilitamos las sesiones para la API
+        $middleware->api(append: [
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ]);
+
+        // Mantenemos tu excepción de CSRF para pruebas
         $middleware->validateCsrfTokens(except: [
             '*',
-        ]); 
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
