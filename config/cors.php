cc6eb500102fb4087fb6806c2f796b15bd9522c2
Paul-Gonz <gonzalezpaul204@gmail.com>
2026-04-27 21:16:13 -0400

    edit cors

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,22 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    // Usamos '*' para que todas las rutas permitan CORS con credenciales
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-    'https://verdu-stock-frontend.vercel.app',
-    'https://verdu-stock-frontend-git-main-paul-gonzs-projects.vercel.app',
+        'https://verdu-stock-frontend.vercel.app',
+        'https://verdu-stock-frontend-git-main-paul-gonzs-projects.vercel.app',
+        'http://localhost:3000', // No lo borres, por si necesitas volver a local
     ],
-
-    'supports_credentials' => true,
 
     'allowed_origins_patterns' => [],
 
